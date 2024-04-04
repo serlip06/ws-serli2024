294f479cfdb1da39ebf045d9f39a68d1cf81b450
Indra Riksa Herlambang <indrariksa@gmail.com>
2024-04-04 22:42:32 +0700

    update db to mongo

--- config/db.go
+++ config/db.go
@@ -11,14 +11,12 @@ var IteungIPAddress string = os.Getenv("ITEUNGBEV1")
 
 var MongoString string = os.Getenv("MONGOSTRING")
 
-var MariaStringAkademik string = os.Getenv("MARIASTRINGAKADEMIK")
-
-var DBUlbimariainfo = atdb.DBInfo{
-	DBString: MariaStringAkademik,
-	DBName:   "db_ulbi",
+var DBUlbimongoinfo = atdb.DBInfo{
+	DBString: MongoString,
+	DBName:   "tesdb2024",
 }
 
-var Ulbimariaconn = atdb.MariaConnect(DBUlbimariainfo)
+var Ulbimongoconn = atdb.MongoConnect(DBUlbimongoinfo)
 
 var Usertables = [4]whatsauth.LoginInfo{mhs, dosen, user, user1}
 
